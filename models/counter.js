21ab2adef5759845a57923165309968d1dc6df81
Batnaidan Tsend <batnaidan@gmail.com>
2021-07-30 17:28:10 +0800

    Counter Mongoose Schema

---
+++ models/counter.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const counterSchema = new Schema({
+  _id: String,
+  sequence_value: Number,
+});
+module.exports = mongoose.model('counters', counterSchema);
