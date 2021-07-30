0b88bdf0d91ae10e4ad377066da94c26ce155e62
Batnaidan Tsend <batnaidan@gmail.com>
2021-07-30 17:28:16 +0800

    Room Mongoose Schema

--- models/room.js
+++ models/room.js
@@ -1,11 +1,15 @@
-import mongoose from 'mongoose';
-import { Schema } from mongoose;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const roomSchema = new Schema({
   _id: { type: Number, default: 100000, required: true },
-  room_isPlaying: {type: Boolean,  default: false},
-  room_deck: Array,
-  room_players: Array,
-  room_created: { type: Date, default: Date.now }
+  room_isPlaying: { type: Boolean, default: false },
+  room_deck: { type: Array, default: [] },
+  room_players: {
+    type: Array,
+    default: [],
+  },
+  room_created: { type: Date, default: Date.now },
+  room_playerLength: { type: Number, default: 0 },
 });
 module.exports = mongoose.model('rooms', roomSchema);
